207294c378352adee2f441973c780448966ed9f1
nhockool1002 <nhut.nguyenminh.it@gmail.com>
2021-12-09 01:02:49 +0700

    Add Test Check

---
+++ myBoard/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from myBoard.core import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+  path('hello/', views.HelloView.as_view(), name='hello'),
+  path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
--- myBoard/urls.py
+++ myBoard/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from myBoard.core import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('hello/', views.HelloView.as_view(), name='hello'),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path(r'api/', include('myBoard.core.urls')),
 ]
