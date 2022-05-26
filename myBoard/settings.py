684a45a0f8ec046669ad156f74cdf4c59897497a
Nhut Nguyen Minh <nhut.nguyenminh.it@gmail.com>
2022-05-26 23:10:33 +0700

    Feature/add licence api (#44)
    * Summary License
    
    * Update
    
    * Update
    
    * Update
    
    * Summary: Fix license api
    
    Co-authored-by: Nhut Nguyen Minh <nhut.nguyen@salto.vn>

--- myBoard/settings.py
+++ myBoard/settings.py
@@ -62,14 +62,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 
